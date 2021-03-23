ee329ac9f015dd875ff2fd38095fed7e01614864
Ibrahim Oguzhan Yilmaz <ibrahimoguzhany@gmail.com>
2021-03-23 16:40:27 +0300

    seeding data

--- btre/urls.py
+++ btre/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('', include('pages.urls')),
     path('listings/', include('listings.urls')),
--- listings/admin.py
+++ listings/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Listing
 
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+  list_display = ('id', 'title', 'is_published','price','list_date','realtor')
+  list_display_links = ('id', 'title')
+  list_filter = ('realtor',)
+
+
+admin.site.register(Listing, ListingAdmin)
 
 
